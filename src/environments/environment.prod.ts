0e9ed945cc11dc8daa7d27b33ffd94d8a6d868b8
TebanV07 <vasqesteban024@gmail.com>
2026-04-02 23:35:13 -0500

    add new domain: Timqu

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   googleClientId: '259954016870-carbome69mftn4pcfirb31qbc6uqq08v.apps.googleusercontent.com',
-  apiUrl: 'https://api.tinko.lat/api/v1',
-  apiBaseUrl: 'https://api.tinko.lat',
+  apiUrl: 'https://api.timqu.lat/api/v1',
+  apiBaseUrl: 'https://api.timqu.lat',
   maxUploadSizeMB: 100,
   videoFormats: ['mp4', 'webm', 'mkv'],
   imageFormats: ['jpg', 'jpeg', 'png', 'gif']
