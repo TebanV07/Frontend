97e35f2f2714b609ee931c89e87d88bd51c20dda
TebanV07 <vasqesteban024@gmail.com>
2026-07-05 14:39:06 -0500

    apiurl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   googleClientId: '259954016870-carbome69mftn4pcfirb31qbc6uqq08v.apps.googleusercontent.com',
 
   //// API Backend
-  apiUrl: 'https://api.timqu.lat/api/v1',
+  apiUrl: 'https://web-production-94f95.up.railway.app/api/v1',
   apiBaseUrl: 'https://web-production-94f95.up.railway.app',
  // Backend local
   //apiUrl: 'http://localhost:8001/api/v1',
