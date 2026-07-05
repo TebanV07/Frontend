cde1972b8d9109688a8eca0418333d6bcbcd17cf
TebanV07 <vasqesteban024@gmail.com>
2026-07-05 15:13:39 -0500

    url otra vez

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   googleClientId: '259954016870-carbome69mftn4pcfirb31qbc6uqq08v.apps.googleusercontent.com',
-  apiUrl: 'https://api.timqu.lat/api/v1',
+  apiUrl: 'https://web-production-94f95.up.railway.app/api/v1',
   apiBaseUrl: 'https://api.timqu.lat',
   maxUploadSizeMB: 100,
   videoFormats: ['mp4', 'webm', 'mkv'],
